2629e71dfd6f2c3a6e7420b629092d64e05f20b2
Zkine <philippe_jaymes@hotmail.com>
2024-01-13 14:09:03 +0100

    Création du fichier dataAbout.json et incrémentation des données page about.jsx

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,64 +1,42 @@
 import Collapse from "../components/collapse";
+import JsnAbout from "../data/dataAbout.json";
 import ImgAbout from "../assets/ImgAbout.png";
 import Banner from "../components/banner";
 import "../styles/main.scss";
 
 function About() {
-  const titre = {
-    fiabilité: "Fiabilité",
-    respect: "Respect",
-    service: "Service",
-    sécurité: "Sécurité",
-  };
-
-  const description = {
-    fiabilité:
-      "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.",
-    respect:
-      "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
-    service:
-      "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N’hésitez pas à nous contacter si vous avez la moindre question.",
-    sécurité:
-      "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l’hôte qu’au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
-  };
-
   return (
     <main>
-      <Banner src={ImgAbout} alt="Très belle montagne">
-        {/* <div className="conteneurabout__conteneurimg"> */}
-        {/* <img
-          src={ImgAbout}
-          alt="Très belle montagne"
-          className="conteneurabout__img"
-        /> */}
-        {/* </div> */}
-      </Banner>
-      <section className="conteneuraboutcollapse">
-        <Collapse
-          titre={titre.fiabilité}
-          description={
-            <p className="collapse_paragraphe">{description.fiabilité}</p>
-          }
-        />
-        <Collapse
-          titre={titre.respect}
-          description={
-            <p className="collapse_paragraphe">{description.respect}</p>
-          }
-        />
-        <Collapse
-          titre={titre.service}
-          description={
-            <p className="collapse_paragraphe">{description.service}</p>
-          }
-        />
-        <Collapse
-          titre={titre.sécurité}
-          description={
-            <p className="collapse_paragraphe">{description.sécurité}</p>
-          }
-        />
-      </section>
+      <Banner src={ImgAbout} alt="Très belle montagne"></Banner>
+      {JsnAbout.map((el) => (
+        <section className="conteneuraboutcollapse" key={el.titre.id}>
+          <Collapse
+            titre={el.titre.fiabilité}
+            description={
+              <p className="collapse_paragraphe">{el.description.fiabilité}</p>
+            }
+          />
+          <Collapse
+            titre={el.titre.respect}
+            description={
+              <p className="collapse_paragraphe">{el.description.respect}</p>
+            }
+          />
+          <Collapse
+            titre={el.titre.service}
+            description={
+              <p className="collapse_paragraphe">{el.description.service}</p>
+            }
+          />
+          <Collapse
+            titre={el.titre.sécurité}
+            description={
+              <p className="collapse_paragraphe">{el.description.sécurité}</p>
+            }
+          />
+        </section>
+      ))}
+      ;
     </main>
   );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -14,13 +14,10 @@ function Home() {
     <main>
       <article>
         <Banner src={ImgHome} alt="la montagne">
-          {/* <div > */}
           <h1 className="styletitre__h1">
             Chez vous, <br className="styletitre__br" />
             partout et ailleurs
           </h1>
-          {/* <img src={ImgHome} alt="la montagne" className="styletitre__img" /> */}
-          {/* </div> */}
         </Banner>
         <section className="logementsection">
           <div className="logementsection__logement">
